3999a0c0cec773c17b67785e172d074f35b836f5
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2026-03-16 16:17:46 +0300

    region 3 comp

---
+++ NetCoreAi.RapidConsume/Program.cs
@@ -0,0 +1,29 @@
+﻿using NetCoreAi.RapidConsume.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "e41189b427mshe98cc942baefd4ep12091djsn49df4792308f" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach (var series in apiSeriesViewModels)
+    {
+        Console.WriteLine($"{series.rank} {series.title} {series.description} {series.rating} {series.year} \n");
+    }
+}
+
+
+Console.ReadLine();
---
+++ NetCoreAi.RapidConsume/ViewModels/ApiSeriesViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAi.RapidConsume.ViewModels
+{
+    public class ApiSeriesViewModel
+    {
+
+
+
+
+
+        public int rank { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string rating { get; set; }
+        public int year { get; set; }
+
+
+    }
+}
