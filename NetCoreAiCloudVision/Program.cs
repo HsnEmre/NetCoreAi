212bc119f3ca81adbb9dda44048c8331b00f9ad1
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2026-03-17 15:27:09 +0300

    loud vision complete

--- NetCoreAiCloudVision/Program.cs
+++ NetCoreAiCloudVision/Program.cs
@@ -1,38 +1,40 @@
-﻿using Google.Cloud.Vision.V1;
+﻿using System.Text.Json;
+using Google.Cloud.Vision.V1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("enter image path");
-
-        Console.WriteLine();
-
+        Console.Write("enter image path: ");
         string imagePath = Console.ReadLine();
 
-        string credentialPath = "this get services json file";
+        string credentialPath = @"C:\Users\Raven\Desktop\Project\mycloudproject-490511-c19c80c32e10.json";
 
         Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+        var json = File.ReadAllText(credentialPath);
+        using var doc = JsonDocument.Parse(json);
+
+        Console.WriteLine("project_id: " + doc.RootElement.GetProperty("project_id").GetString());
+        Console.WriteLine("client_email: " + doc.RootElement.GetProperty("client_email").GetString());
+        Console.WriteLine();
 
         try
         {
             var client = ImageAnnotatorClient.Create();
-
             var image = Image.FromFile(imagePath);
             var response = client.DetectText(image);
-            Console.WriteLine("text in the image");
-            Console.WriteLine();
-            foreach (var anotation in response)
+
+            Console.WriteLine("text in image:");
+            foreach (var a in response)
             {
-                if (!string.IsNullOrEmpty(anotation.Description))
-                {
-                    Console.WriteLine($"{anotation.Description}");
-                }
+                if (!string.IsNullOrWhiteSpace(a.Description))
+                    Console.WriteLine(a.Description);
             }
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"an  error occured {ex.Message}");
+            Console.WriteLine("error: " + ex.Message);
         }
     }
 }
