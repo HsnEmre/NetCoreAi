0b9704923f803518d8ca0bcf89babf155f33bb9d
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2026-03-17 12:50:49 +0300

    cloudvision

---
+++ NetCoreAiCloudVision/Program.cs
@@ -0,0 +1,38 @@
+﻿using Google.Cloud.Vision.V1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter image path");
+
+        Console.WriteLine();
+
+        string imagePath = Console.ReadLine();
+
+        string credentialPath = "this get services json file";
+
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+
+        try
+        {
+            var client = ImageAnnotatorClient.Create();
+
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            Console.WriteLine("text in the image");
+            Console.WriteLine();
+            foreach (var anotation in response)
+            {
+                if (!string.IsNullOrEmpty(anotation.Description))
+                {
+                    Console.WriteLine($"{anotation.Description}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"an  error occured {ex.Message}");
+        }
+    }
+}
