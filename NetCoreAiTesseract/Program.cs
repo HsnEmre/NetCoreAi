a3a50c7ae6f51ee313dfe1b5612c56eeed27a79d
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2026-03-17 12:32:05 +0300

    project 7 tesseract complete

---
+++ NetCoreAiTesseract/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("the image path for charachter reading");
+        string imagepath = Console.ReadLine();
+
+        string tessDataPath = @"C:\tessdata";
+
+        try
+        {
+            using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagepath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("text read from the image ");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"bir hata olustu :{ex.Message}");
+        }
+        Console.ReadLine();
+    }
+}
